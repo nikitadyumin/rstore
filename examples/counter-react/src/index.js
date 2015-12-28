f48476ec8f4e394e9a6f43b51c72708290a9140e
ndyumin <nikita.dyumin@gmail.com>
2015-12-29 00:13:30 +0500

    add react example

---
+++ examples/counter-react/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Bacon from 'baconjs';
+import { render  as renderDom } from 'react-dom';
+import { store } from 'rstore';
+
+const Counter = ({model, onAdd, onSub}) => (
+    <div>
+        <div>{model}</div>
+        <button onClick={onAdd}>+</button>
+        <button onClick={onSub}>-</button>
+    </div>
+);
+
+const dispatcher = () => {
+    const bus = new Bacon.Bus();
+    return ({
+        stream: () => bus,
+        add: () => bus.push(1),
+        sub: () => bus.push(-1)
+    });
+};
+
+const _dispatcher = dispatcher();
+
+const render = (model) => {
+    renderDom(
+        <Counter model={model} onAdd={_dispatcher.add} onSub={_dispatcher.sub}/>,
+        document.getElementById('root')
+    )
+};
+
+const _store = store(0)
+    .plug(_dispatcher.stream(), (s, a) => s + a)
+    .stream().onValue(render);
