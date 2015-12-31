2c5e44545c9c8fc1967087c731483f7f6ba6b35c
ndyumin <nikita.dyumin@gmail.com>
2015-12-31 08:53:35 +0500

    update todo example with lenses from rstore

--- examples/todo/src/index.js
+++ examples/todo/src/index.js
@@ -7,48 +7,12 @@ require('todomvc-app-css/index.css');
 
 const $ = require('jquery');
 const Bacon = require('baconjs');
-const rstore = require('rstore').store;
+const rs = require('rstore');
 
 // UTIL
 
 function clone(o) {
     return JSON.parse(JSON.stringify(o));
-}
-
-function defineGetter(name) {
-    return o => o[name];
-}
-
-function defineSetter(name) {
-    return (o, v) => {
-        const _clone = clone(o);
-        _clone[name] = v;
-        return _clone;
-    };
-}
-
-function lens(name) {
-    function _lens(getter, setter) {
-        return {
-            get: getter,
-            set: setter,
-            toggle: obj => setter(obj, !getter(obj)),
-            combine: l => _lens(
-                (obj) => l.get(getter(obj)),
-                (obj, val) => setter(obj, l.set(getter(obj), val))
-            ),
-            defineMapSetter: mapper => _lens(
-                getter,
-                (obj, value) => setter(obj, getter(obj).map(mapper(value)))
-            ),
-            defineFilterSetter: filter => _lens(
-                getter,
-                (obj, value) => setter(obj, getter(obj).filter(filter(value)))
-            )
-        };
-    }
-
-    return _lens(defineGetter(name), defineSetter(name));
 }
 
 // ACTIONS
@@ -112,7 +76,7 @@ const render = model => {
 
 // STORE
 
-const model = rstore({
+const model = rs.store({
     maxId: 1,
     filter: '',
     tasks: [
@@ -123,9 +87,9 @@ const model = rstore({
 
 // DISPATCHERS
 
-const filterL = lens('filter');
-const tasksL = lens('tasks');
-const doneL = lens('done');
+const filterL = rs.lens('filter');
+const tasksL = rs.lens('tasks');
+const doneL = rs.lens('done');
 
 const removeByIdL = tasksL
     .defineFilterSetter(action => task => task.id !== action);
