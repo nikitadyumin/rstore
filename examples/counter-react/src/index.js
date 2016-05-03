4b275ba82c66a820356e0cdd3e9303bdda680a58
ndyumin <nikita.dyumin@gmail.com>
2016-05-03 14:09:27 +0500

    update counter react example (use internal bus)

--- examples/counter-react/src/index.js
+++ examples/counter-react/src/index.js
@@ -1,34 +1,24 @@
 import React from 'react';
-import Bacon from 'baconjs';
 import { render  as renderDom } from 'react-dom';
-import { store } from 'rstore';
+import { store, bus } from 'rstore';
 
 const Counter = ({model, onAdd, onSub}) => (
     <div>
-        <div>{model}</div>
-        <button onClick={onAdd}>+</button>
         <button onClick={onSub}>-</button>
+        <span>{model}</span>
+        <button onClick={onAdd}>+</button>
     </div>
 );
 
-const dispatcher = () => {
-    const bus = new Bacon.Bus();
-    return ({
-        stream: () => bus,
-        add: () => bus.push(1),
-        sub: () => bus.push(-1)
-    });
-};
-
-const _dispatcher = dispatcher();
+const updates$ = bus();
 
 const render = (model) => {
     renderDom(
-        <Counter model={model} onAdd={_dispatcher.add} onSub={_dispatcher.sub}/>,
+        <Counter model={model} onAdd={() => updates$.next(1)} onSub={() => updates$.next(-1)}/>,
         document.getElementById('root')
     )
 };
 
 store(0)
-    .plug(_dispatcher.stream(), (s, a) => s + a)
+    .plug(updates$, (s, a) => s + a)
     .subscribe(render);
