b5aba35043e874dc00a00e95137a9144ba756786
ndyumin <nikita.dyumin@gmail.com>
2016-06-28 09:54:28 +0500

    improve symbol ponyfill

--- src/store/constants.js
+++ src/store/constants.js
@@ -1,6 +1,13 @@
 /**
  * Created by ndyumin on 06.06.2016.
  */
+"use strict";
+
+function $$Symbol(name) {
+    this.name = name;
+}
+
+$$Symbol.prototype.toString = () => Object.keys(this).map(k => `${k}:${this[k]}`).join(',');
 
 const symbol = typeof Symbol !== 'undefined' ? Symbol : name => ({name});
 
