ed5152f19a335e4c0de0cd4b57daf57668f04ade
ndyumin <nikita.dyumin@gmail.com>
2016-09-29 15:40:08 +0500

    - add babel for dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,6 @@ module.exports = {
         loaders: [
             {
                 test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
                 loader: 'babel-loader'
             }
         ]
