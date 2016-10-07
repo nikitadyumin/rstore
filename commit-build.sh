ce30cc55f4254c2b5156c8033e30280974139d19
ndyumin <nikita.dyumin@gmail.com>
2016-10-07 18:03:33 +0500

    - build with travis / add email

--- commit-build.sh
+++ commit-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ev
+git config --global user.email "dyuminnikita@gmail.com"
+git config --global user.name "Travis CI"
 git status
 git add ./dist/rstore.js
 git commit -m "add bundle"
